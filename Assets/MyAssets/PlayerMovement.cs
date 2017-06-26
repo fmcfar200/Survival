375ce08d6f36a76b381aedf4116d7ef0d64ac0a9
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-06-26 15:57:16 +0100

    commit

---
+++ Assets/MyAssets/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private GameObject player;
+    Animator playerAnimator;
+    Transform playerTransform;
+
+    private float runSpeed;
+    private bool moving;
+
+    private string verticalString = "Vertical";
+    private string horizontalString = "Horizontal";
+    private float hor;
+    private float vert;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player!= null)
+        {
+            playerAnimator = player.GetComponent<Animator>();
+            playerTransform = player.transform;
+        }
+    }
+
+    void Start()
+    {
+        runSpeed = 1.0f;
+        moving = false;
+
+        
+
+    }
+
+    void Update()
+    {
+        hor = Input.GetAxis(horizontalString);
+        vert = Input.GetAxis(verticalString);
+
+        if (vert > 0)
+        {
+            playerAnimator.SetBool("Moving", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Moving", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (vert > 0 || vert < 0)
+        {
+            playerTransform.Translate(Vector3.forward * runSpeed * vert * Time.deltaTime);
+            
+        }
+        else if (hor > 0 || hor < 0)
+        {
+            playerTransform.Translate(Vector3.right * runSpeed * hor * Time.deltaTime);
+
+        }
+
+    }
+}
