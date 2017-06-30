de9af4782ce26ae794f5fd8a931601a058f86bb7
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-06-30 16:41:23 +0100

    Health and UI

---
+++ Assets/MyAssets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public float currentHealth;
+    float maxHealth = 100f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+    }
+
+    public void Death()
+    {
+        if (gameObject.tag == "Player")
+        {
+            print("Dead");
+        }
+    }
+}
---
+++ Assets/UIScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UIScript : MonoBehaviour {
+
+    GameObject player;
+    Health health;
+    PlayerCombat combat;
+
+    public Image healthBar;
+    public Text ammoText;
+
+
+    void Start()
+    {
+        InitPlayerComponents();
+    }
+
+    void Update()
+    {
+        UpdateUI();
+
+    }
+
+    void UpdateUI()
+    {
+        healthBar.fillAmount = health.currentHealth / 100.0f;
+        ammoText.text = combat.clip.ToString() + "/" + combat.ammo.ToString();
+    }
+
+    void InitPlayerComponents()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            health = player.GetComponent<Health>();
+            combat = player.GetComponent<PlayerCombat>();
+        }
+        else
+        {
+            Debug.LogError("Player Not Found!");
+        }
+    }
+
+}
