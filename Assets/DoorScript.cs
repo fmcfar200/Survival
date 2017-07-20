46e034388280c3cb3c29873d69a91ff1a74fcd55
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-07-20 21:02:38 +0100

    bleh2

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool locked = false;
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("joystick button 3"))
+            {
+                //open door
+                OpenDoor();
+            }
+        }
+    }
+
+    void OpenDoor()
+    {
+        Transform doorTrans = this.transform;
+        doorTrans.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    }
+    
+}
